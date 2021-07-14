81976756d1460475fad3a5037a35ec5837c664ad
Gluttony-Meow <glutony0910@gmail.com>
2021-07-14 22:31:08 +0700

    Initial commit

---
+++ Models/Model1.cs
@@ -0,0 +1,20 @@
+namespace WebApplication1.Models
+{
+    using System;
+    using System.Data.Entity;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Linq;
+
+    public partial class Model1 : DbContext
+    {
+        public Model1()
+            : base("name=StudentModel")
+        {
+        }
+        public DbSet<Student> Students { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+        }
+    }
+}
---
+++ Models/Student.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class Student
+    {
+        [Key]
+        public int StudentId { get; set; }
+
+        [StringLength(maximumLength: 150, MinimumLength = 2)]
+        public string StudentName { get; set; }
+
+
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
+        public DateTime StudentDOB { get; set; }
+
+        [Required]
+        public String Address { get; set; }
+
+        [Required]
+        public String Picture { get; set; }
+    }
+}
